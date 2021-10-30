468895a5acc9112591f0d7708e6be99a5d329b43
MirrorBottle <setiawanbayu66152@gmail.com>
2021-10-30 13:46:38 +0800

    init

---
+++ askit/views/__init__.py
@@ -0,0 +1,3 @@
+from . import home
+from . import login
+from . import register
---
+++ askit/views/register.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+# UTILS
+from utils import LARGEFONT
+
+# PAGES
+from . import home, login 
+from tkinter import ttk
+
+
+class Register(tk.Frame):
+  def __init__(self, parent, controller):
+      tk.Frame.__init__(self, parent)
+        
+      # label of frame Layout 2
+      label = ttk.Label(self, text ="Startpage", font = LARGEFONT)
+        
+      # putting the grid in its place by using
+      # grid
+      label.grid(row = 0, column = 4, padx = 10, pady = 10)
+
+      button1 = ttk.Button(self, text ="Home",
+      command = lambda : controller.show_frame(home.Home))
+    
+      # putting the button in its place by
+      # using grid
+      button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+      ## button to show frame 2 with text layout2
+      button2 = ttk.Button(self, text ="Login",
+      command = lambda : controller.show_frame(login.Login))
+    
+      # putting the button in its place by
+      # using grid
+      button2.grid(row = 2, column = 1, padx = 10, pady = 10)
